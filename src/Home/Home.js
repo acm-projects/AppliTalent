7f578c1b09fa046d575541c7cb706abc6e08b361
yeeric01 <yeeric01@gmail.com>
2021-03-14 17:02:06 -0500

    added homepage

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,10 +6,25 @@ import React from 'react';
 const Home = () => {
     return (
       <div className = "wholeHome">
-        <div className = "top"></div>
-        <p>In the homepage</p>
-        <button onClick={()=>firebase.auth().signOut()}>Sign out</button>
+        <div className = "topBar">
+        <img src="logo.png" width="80" height="80" alt="Computer Hope"></img>
+          <div className="webNameDiv">
+            <label className="webName">GoHire</label>
+          </div>
+        {/*<img src="NotifBell.png" width="100" height="80" alt="Computer Hope"></img>
+          <button onClick={flipLogin} className="signUpButton">Sign up</button>
+          
+          
+          <img src="logo.png" width="80" height="80" alt="Computer Hope"></img>
+            <p>
+              GoHire
+            </p>
+          <div className="NotifBell.png"></div>*/}
+        
       </div>
+      
+      </div>
+      
     );
   }
   
