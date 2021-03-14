88a635735c2772a2e3a21f11276cc3d9a5e6292c
yeeric01 <yeeric01@gmail.com>
2021-03-14 15:45:51 -0500

    fixed error messages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Login from "./Login/Login.js"
 import Home from "./Home/Home.js"
+import AddCard from "./AddCard.js"
 import {AuthProvider} from "./Context/Auth.js"
 import PrivateRoute from "./PrivateRoute.js"
 import { BrowserRouter as Router, Route} from "react-router-dom";
@@ -16,9 +17,8 @@ const App = () => {
             <PrivateRoute exact path="/" exact component={Home} />
             <Route exact path="/login" exact component={Login} />
           </div>
-          
         </Router>
-      </AuthProvider>    
+      </AuthProvider>
     );
 }
 
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -10,13 +10,13 @@ const Login = ({ history }) => {
   const [isFlipping, setIsFlipping] = useState(false);
   const handleSignUp = useCallback(async event => {
     event.preventDefault();
+    clearSignupError();
     const { email, password } = event.target.elements;
-    try {
-      await firebase
-        .auth()
-        .createUserWithEmailAndPassword(email.value, password.value);
-      history.push("/");
-    } catch (err) {
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+    .then(()=>{
+      history.push("/")
+    })
+    .catch(err=>{
       switch(err.code){
         case "auth/email-already-in-use":
         case "auth/invalid-email":
@@ -26,30 +26,42 @@ const Login = ({ history }) => {
           document.getElementsByClassName("pswdInvalid2")[0].style.opacity = 1;
           return;
       }
-    }
+    });
   }, [history]);
 
   const handleLogin = useCallback(
     async event => {
+      clearLoginError();
       event.preventDefault();
       const { email, password } = event.target.elements;
-      try {
-        await firebase
-          .auth()
-          .signInWithEmailAndPassword(email.value, password.value);
+      firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+      .then(() => {
         history.push("/");
-      } catch (err) {
-          switch(err.code){
-            case "auth/invalid-email":
-            case "auth/user-disabled":
-            case "auth/user-not-found":
-              document.getElementsByClassName("emailInvalid")[0].style.opacity = 1;
-              break;
-            case "auth/wrong-password":
-              document.getElementsByClassName("pswdInvalid")[0].style.opacity = 1;
-              break;
-          }
-      }
+      })
+      .catch(err => {
+        console.log("HEY");
+        console.log(email.value + "  " + password.value);
+        console.log(err.code);
+        switch(err.code){
+          case "auth/invalid-email":
+            console.log("HEY1");
+          case "auth/user-disabled":
+            console.log("HEY2");
+          case "auth/user-not-found":
+            console.log("HEY3");
+            document.getElementsByClassName("emailInvalid")[0].style.opacity = 1;
+            break;
+          case "auth/wrong-password":
+            console.log("HEY4");
+            document.getElementsByClassName("pswdInvalid")[0].style.opacity = 1;
+            break;
+        }
+        console.log()
+        if(document.getElementsByClassName("text2")[0].value==="")
+          document.getElementsByClassName("text2")[0].placeholder = "Please Enter Password";
+        else
+          document.getElementsByClassName("text2")[0].placeholder = "Password";
+      });
     },
     [history]
   );
@@ -60,7 +72,8 @@ const Login = ({ history }) => {
     if(isFlipping === true){
       return;
     }
-      
+    clearLoginError();
+    clearSignupError();
     let loginBlock = document.getElementsByClassName("loginBlock")[0];
     let signButton = document.getElementsByClassName("signUpButton")[0];
     if(isLogin === true){
