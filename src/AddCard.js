10fa0ac4ae6cd6af6ba3ca9642081da71bdbe38e
yeeric01 <yeeric01@gmail.com>
2021-03-14 13:34:49 -0500

    added card

---
+++ src/AddCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const AddCard = () => {
+
+
+  
+      return (
+        <div className = "wholeHome">
+          <div className = "top"></div>
+          <p>In the addCard</p>
+        </div>
+      );
+}
+  
+  export default AddCard;
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from "./Login/Login.js"
 import Home from "./Home/Home.js"
 import {AuthProvider} from "./Context/Auth.js"
+import AddCard from "./AddCard.js"
 import PrivateRoute from "./PrivateRoute.js"
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
@@ -12,10 +13,15 @@ const App = () => {
     return (
       <AuthProvider>
         <Router>
+          <Route exact path="/" exact component={AddCard} />
+          
+          {/*
+          <Route exact path="/" exact component={Home} />
           <div className="App">
             <PrivateRoute exact path="/" exact component={Home} />
             <Route exact path="/login" exact component={Login} />
           </div>
+          */}
         </Router>
       </AuthProvider>    
     );
