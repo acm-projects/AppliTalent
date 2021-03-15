0e85c559fcc9c226cf3640fc556d244fb6412157
yeeric01 <yeeric01@gmail.com>
2021-03-15 11:10:52 -0500

    newest

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,22 +6,11 @@ import React from 'react';
 const Home = () => {
     return (
       <div className = "wholeHome">
-        <div className = "topBar">
-        <img src="logo.png" width="80" height="80" alt="Computer Hope"></img>
-          <div className="webNameDiv">
-            <label className="webName">GoHire</label>
-          </div>
-        {/*<img src="NotifBell.png" width="100" height="80" alt="Computer Hope"></img>
-          <button onClick={flipLogin} className="signUpButton">Sign up</button>
-          
-          
-          <img src="logo.png" width="80" height="80" alt="Computer Hope"></img>
-            <p>
-              GoHire
-            </p>
-          <div className="NotifBell.png"></div>*/}
+        <div className = "homeTopBar">
+          <img src="logo.png"></img>
+          <img src="notiBell.png"></img>
         
-      </div>
+        </div>
       
       </div>
       
