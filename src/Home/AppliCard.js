cafc11f63f8bc69770128b3c5581e08d8ba4a7b0
yeeric01 <yeeric01@gmail.com>
2021-03-17 15:36:02 -0500

    added applicard

---
+++ src/Home/AppliCard.js
@@ -0,0 +1,13 @@
+import './appliCard.css';
+import React from 'react';
+
+
+const AppliCard = () => {
+  return (
+    <div className = "wholeHome">
+    
+    </div>
+    );
+}
+  
+  export default AppliCard;
